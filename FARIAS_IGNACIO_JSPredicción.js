bb06de653029de55c3711a44d124054210c67245
FranciscaMB <ff.montecino.programacion.iccp@gmail.com>
2022-08-19 10:44:16 -0400

    subiendo contenido.

---
+++ FARIAS_IGNACIO_JSPredicción.js
@@ -0,0 +1,24 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//el console.log imprime: Nací en 1980
+
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+    var EntradaAñoNacimiento= 1980;
+    //el console.log imprime:Nací en 1980
+
+
+    function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is: " + num1);
+            console.log("num2 is: " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+        //el console.log imprime: ¡Sumando números!
+        //el console.log imprime: num1 is: 10
+    //el console.log imprime: num2 is: 20
+    //el console.log imprime: 30
---
+++ Prediccion_de_codigo_2.js
@@ -0,0 +1,23 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+//la funcion me va a mostar el arreglo completo
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//la funcion me va a mostrar: var frozen [brocoli, helado,croqueta de papa]
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+//creamos un arreglo llamado movieLibraty.
+//agregamos un valor llamado "zoro".
+//remplazamos E.T. por Zoro.
+//mostramos el arreglo.
