ad1e15cf95684da27faef97edb87b1ca9aa35908
FranciscaMB <ff.montecino.programacion.iccp@gmail.com>
2022-08-19 12:05:45 -0400

    subiendo actividad

---
+++ FARIAS_IGNACIO_JS_JUGUETEO.js
@@ -0,0 +1,10 @@
+var alturaNIÑO = 18;
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño){
+
+    if (alturaNiño >52) {
+        console.log("¡Súbete, chico!");      }
+        else {
+            console.log("Lo siento, chico. Tal vez el próximo año");
+                    }
+                
+                }
